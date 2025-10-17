0abf447d48af6ad626557ae9e453cc5e850e1796
DevAlex001 <85975703+DevAlex001@users.noreply.github.com>
2025-10-17 18:58:50 -0400

    Update Lambda-bedrock.py

--- Lambda-bedrock.py
+++ Lambda-bedrock.py
@@ -5,12 +5,19 @@ def lambda_handler(event, context):
     client = boto3.client(service_name="bedrock-runtime")
 
     messages = [
-        {"role": "user", "content": [{"text": "What is AWS?"}]},
+        {"role": "user", "content": [{"text": "What is automation?"}]},
     ]
 
+    guardrail_config = {
+        "guardrailIdentifier": "arn:aws:bedrock:us-east-2:340752832578:guardrail/ygjpsqisew0z",
+        "guardrailVersion": "1",
+        "trace": "enabled",
+    }
+
     model_response = client.converse(
-        modelId="us.amazon.nova-lite-v1:0", 
-        messages=messages
+        modelId="us.amazon.nova-lite-v1:0",
+        messages=messages,
+        guardrailConfig=guardrail_config
     )
-  
+
     return model_response["output"]["message"]["content"][0]["text"]
